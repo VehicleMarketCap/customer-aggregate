c4c27850277bce306464238bb30505a8c4aa90a2
VehicleMarketCap <project331w@gmail.com>
2022-04-07 18:43:32 +0300

    * Added Junit dependency; * Introduced two main test classes - CustomerControllerITest and CustomerServiceTest; * Added 3 main empty test cases for CustomerServiceTest;

---
+++ src/test/java/com/customer/service/customeraggregate/controller/CustomerControllerITest.java
@@ -0,0 +1,4 @@
+package com.customer.service.customeraggregate.controller;
+
+public class CustomerControllerITest {
+}
---
+++ src/test/java/com/customer/service/customeraggregate/service/CustomerServiceTest.java
@@ -0,0 +1,24 @@
+package com.customer.service.customeraggregate.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CustomerServiceTest {
+
+    @Test
+    public void create_returnsCreatedCustomer() {
+
+    }
+
+    @Test
+    public void getAll_whenCustomersExist_returnsAll() {
+
+    }
+
+    @Test
+    public void getAll_whenCustomersDoesntExist_returnsEmptyList() {
+
+    }
+}
