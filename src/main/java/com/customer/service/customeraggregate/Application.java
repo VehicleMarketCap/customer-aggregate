c922d2a4f6467f7acf952be8a503ddcfca435b15
VehicleMarketCap <project331w@gmail.com>
2022-04-07 18:30:05 +0300

    Renamed main application file

--- src/main/java/com/customer/service/customeraggregate/Application.java
+++ src/main/java/com/customer/service/customeraggregate/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerAggregateApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerAggregateApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
