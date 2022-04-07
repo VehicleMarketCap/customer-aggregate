e914562882100b066120d11c6d307504bb716779
VehicleMarketCap <project331w@gmail.com>
2022-04-07 18:25:53 +0300

    Initial commit

---
+++ src/main/java/com/customer/service/customeraggregate/CustomerAggregateApplication.java
@@ -0,0 +1,13 @@
+package com.customer.service.customeraggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAggregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAggregateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/service/customeraggregate/CustomerAggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.service.customeraggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAggregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
